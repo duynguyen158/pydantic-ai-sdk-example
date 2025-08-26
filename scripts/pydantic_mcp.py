66c05bf20561a93cac8d9744b1d959ba81211ea3
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2025-08-26 17:49:45 -0400

    [add] working mcp example

--- scripts/pydantic_mcp.py
+++ scripts/pydantic_mcp.py
@@ -13,19 +13,19 @@ from pydantic_ai.providers.ollama import OllamaProvider
 class FactsAboutDuy(BaseModel):
     politics: Annotated[bool, Field(description="Is Duy into politics?")]
     bagels: Annotated[bool, Field(description="Is Duy into bagels?")]
-    addition: Annotated[
-        bool, Field(description="Use the add tool to tell me the sum of 451 and 227.")
-    ]
+    # addition: Annotated[
+    #     bool, Field(description="Use the add tool to tell me the sum of 451 and 227.")
+    # ]
     rationale: Annotated[
         str,
         Field(
-            description="Explain, in a few sentences, your thinking in answering the questions above."
+            description="Explain briefly your thinking in answering the questions above."
         ),
     ]
 
 
 async def main() -> None:
-    server = MCPServerStreamableHTTP(url="http://localhost:8000")
+    server = MCPServerStreamableHTTP(url="http://localhost:8000/mcp")
     # Ollama's API is OpenAI-compatible
     model = OpenAIChatModel(
         model_name="qwen3:4b-thinking-2507-q4_K_M",
@@ -34,12 +34,14 @@ async def main() -> None:
     agent = Agent(
         model,
         output_type=FactsAboutDuy,
-        system_prompt="You are a Gen Z assistant. Answer questions as provided and append three emojis to end of each of your answers.",
+        system_prompt="Answer questions as provided.",
         toolsets=[server],
     )
 
     async with agent:
-        result = await agent.run("The windy city in the US of A.")
+        result = await agent.run(
+            "Answer questions as provided in the output structure."
+        )
 
     print("Result type:", type(result.output))
     print("Result:", result.output.model_dump_json(indent=2))
